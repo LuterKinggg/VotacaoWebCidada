b68dd24f639e115ed9bee7c4ad5f3a795c8c84be
FelipeFMedeiros <felipeff_freitas@outlook.com>
2023-11-26 21:55:34 -0300

    Commitando minhas alterações locais

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
+app.secret_key = 'secretkey'
+
+# Configuração para servir arquivos estáticos
+app.config['STATIC_FOLDER'] = 'static'
 
 votes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -14,8 +18,15 @@ def resultado():
 
 @app.route("/voto", methods=["POST"])
 def voto():
+    selection = request.form.get("option")
+    if selection is None:
+        flash("Selecione uma opção")
+        return redirect("/")
     selection = int(request.form.get("option"))
     votes[selection] += 1
     print(selection)
     print(votes)
-    return redirect("/resultado")
+    return redirect("/resultado") 
+
+if __name__ == "__main__":
+    app.run(debug=True)
