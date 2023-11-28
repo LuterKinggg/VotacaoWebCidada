3b40e474b99ffa04d4f305351dad945f3594007d
FelipeFMedeiros <felipeff_freitas@outlook.com>
2023-11-27 22:06:54 -0300

    PDF Button

--- static/pdf.js
+++ static/pdf.js
@@ -1,10 +1,32 @@
-function openPDF() {
+function openPDF(documentName) {
     var pdfViewer = document.getElementById("pdfViewer");
     pdfViewer.style.display = "block";
-    pdfViewer.src = "../static/teste.pdf";
+    pdfViewer.src = getPDFPath(documentName);
+    console.log("Abrindo " + getPDFPath(documentName));
 }
 
-// Adicione um evento para fechar o PDF se desejado
-document.getElementById("pdfViewer").addEventListener("click", function() {
+// Fechar o PDF se 
+/*document.getElementById("pdfViewer").addEventListener("click", function() {
     this.style.display = "none";
-});
+});*/
+
+function getPDFPath(documentName) {
+    var documentPaths = {
+        "O Mundo Sombrio de Sabrina": "../static/teste.pdf",
+        "O Mundo dos Animais": "../static/teste.pdf",
+        "Hidrate seu Pet": "../static/teste.pdf",
+        "Animais em Extinção": "../static/teste.pdf",
+        "Espécies de Peixes": "../static/teste.pdf",
+        "Gato Minhoso": "../static/teste.pdf",
+        "DivertidaMentes de uma Bailarina": "../static/teste.pdf",
+        "Espécies de Gatos": "../static/teste.pdf",
+        "Previsão do tempo no Litoral Leste, no Agreste e no Vale do Assú": "../static/teste.pdf",
+        "Lingua Brasileira de Sinais (Libras)": "../static/teste.pdf",
+        "Lei aprovada em SP": "../static/teste.pdf",
+        "Gravidez silenciosa": "../static/teste.pdf",
+        "O Pequeno Príncipe": "../static/teste.pdf",
+        "Porque você deveria adotar um animal": "../static/teste.pdf",
+    };
+
+    return documentPaths[documentName];
+}
